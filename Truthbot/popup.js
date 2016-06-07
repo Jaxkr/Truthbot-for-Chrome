2d61a6ea74f649a86ff05d82ee5f72663d7b6f10
Jackson Roberts <jacksonroberts25@gmail.com>
2016-06-07 15:29:28 -0600

    sync

--- Truthbot/popup.js
+++ Truthbot/popup.js
@@ -1,6 +1,7 @@
 var api_base_url = 'http://localhost:8000/api/';
 function sendRequest(url, callback) {
 	var request = new XMLHttpRequest();
+	console.log('queried: ' + api_base_url + url);
 	request.open('GET', api_base_url + url, true);
 
 	request.onload = function() {
@@ -28,14 +29,38 @@ function init() {
 }
 
 function queryAPI(currentTabURL) {
-	sendRequest('getsiteinfo/?url=' + encodeURIComponent(currentTabURL), displayPageInfo);
+	sendRequest('getownerinfo/?url=' + encodeURIComponent(currentTabURL), displayPageInfo);
+	sendRequest('getarticleinfo/?url=' + encodeURIComponent(currentTabURL), displayArticleInfo);
 }
 
 function displayPageInfo(data) {
 	console.log(data);
-	document.getElementById('loadedcontent').style.display = 'block';
-	document.getElementById('loading').style.display = 'none';
+	if (data['status'] == 'success') {
+		var ownership_sentence = '<b>' + data['domain'] + '</b> is part of <b>' + data['organization'][0]['fields']['name'] + '</b>';
+		if (data['parent_organizations'].length > 0) {
+			ownership_sentence += ' which is owned by <b>' + data['parent_organizations'][0]['fields']['name'] + '</b>.'
+		}
+		document.getElementById('ownership').innerHTML = ownership_sentence;
+		document.getElementById('loadedcontent').style.display = 'block';
+	} else if (data['status'] == 'notfound') {
+		document.getElementById('noownerinfo').style.display = 'block';
 
+	}
+	document.getElementById('loadingowners').style.display = 'none';
+
+}
+
+function displayArticleInfo(data) {
+	console.log(data);
+	if (data['status'] == 'first') {
+		document.getElementById('first').style.display = 'block';
+	} else if (data['status'] == 'success') {
+		document.getElementById('articletitle').innerHTML = data['article'][0]['fields']['title'];
+		document.getElementById('articlesummary').innerHTML = data['article'][0]['fields']['summary'];
+		document.getElementById('keywords').innerHTML = data['article'][0]['fields']['keywords'];
+		document.getElementById('articleinfo').style.display = 'block';
+	}
+	document.getElementById('loadingarticle').style.display = 'none';
 }
 
 
