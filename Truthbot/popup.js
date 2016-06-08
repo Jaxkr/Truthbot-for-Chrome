06f86b61601a16d9c4f6e459a94cf86c51a534fc
Jackson Roberts <jacksonroberts25@gmail.com>
2016-06-08 00:20:00 -0600

    almost functional

--- Truthbot/popup.js
+++ Truthbot/popup.js
@@ -34,13 +34,14 @@ function queryAPI(currentTabURL) {
 }
 
 function displayPageInfo(data) {
-	console.log(data);
 	if (data['status'] == 'success') {
-		var ownership_sentence = '<b>' + data['domain'] + '</b> is part of <b>' + data['organization'][0]['fields']['name'] + '</b>';
-		if (data['parent_organizations'].length > 0) {
-			ownership_sentence += ' which is owned by <b>' + data['parent_organizations'][0]['fields']['name'] + '</b>.'
+		var sentence = '<b>' + data['domain'] + '</b> is part of <b>' + data['organization'][0]['fields']['name'] + '</b>';
+		for (var i = 0; i < data['parents'].length; i++) {
+			console.log(data['parents'][i][0]['fields']['name']);
+			sentence += ', which is owned by <b>' + data['parents'][i][0]['fields']['name'] + '</b>'
 		}
-		document.getElementById('ownership').innerHTML = ownership_sentence;
+		sentence += '.'
+		document.getElementById('ownership').innerHTML = sentence;
 		document.getElementById('loadedcontent').style.display = 'block';
 	} else if (data['status'] == 'notfound') {
 		document.getElementById('noownerinfo').style.display = 'block';
@@ -55,10 +56,12 @@ function displayArticleInfo(data) {
 	if (data['status'] == 'first') {
 		document.getElementById('first').style.display = 'block';
 	} else if (data['status'] == 'success') {
+		/*
 		document.getElementById('articletitle').innerHTML = data['article'][0]['fields']['title'];
 		document.getElementById('articlesummary').innerHTML = data['article'][0]['fields']['summary'];
 		document.getElementById('keywords').innerHTML = data['article'][0]['fields']['keywords'];
 		document.getElementById('articleinfo').style.display = 'block';
+		*/
 	}
 	document.getElementById('loadingarticle').style.display = 'none';
 }
