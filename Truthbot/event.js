87784931ccb6708800453010202704e3e66178cd
Jackson Roberts <jacksonroberts25@gmail.com>
2016-07-30 20:10:24 -0600

    changed domain

--- Truthbot/event.js
+++ Truthbot/event.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var base_truthbot_url = 'http://localhost:8000/articles/article/';
+	var base_truthbot_url = 'http://www.truthbot.org/articles/article/';
 	var url = base_truthbot_url + encodeURIComponent(tab.url);
 	chrome.tabs.create({'url': url}, function(tab) {
 	});
