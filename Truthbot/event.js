116c0459cab586a1a98aeb256a2446fdfb2627c3
Jackson Roberts <jacksonroberts25@gmail.com>
2016-08-03 14:52:25 -0600

    deleted

--- Truthbot/event.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var base_truthbot_url = 'https://www.truthbot.org/articles/article/';
-	var url = base_truthbot_url + encodeURIComponent(tab.url);
-	chrome.tabs.create({'url': url}, function(tab) {
-	});
-});
